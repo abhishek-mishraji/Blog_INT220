5e159002e047f9532f7f5916d4c5615084a00731
Abhishek Mishra <95057637+abhishek-mishraji@users.noreply.github.com>
2024-11-17 15:40:48 +0530

    Update constants.php

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-define("ROOT_URL", "http://localhost/blog/");
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'blog');
+define("ROOT_URL", "https://vivekpandeyblog-ac22f5318f84.herokuapp.com/");
+define('DB_HOST', 'xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_USER', 'vcf900not8pc0upr');
+define('DB_PASS', 'elrpdu6o0sbaoz54');
+define('DB_NAME', 'kqszyk3cwshgko7s');
 if (!isset($_SESSION['user-id'])) {
     header("location: " . ROOT_URL . "logout.php");
     //destroy all sessions and redirect user to login page
