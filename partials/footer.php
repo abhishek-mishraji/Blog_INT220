6222ec7f314d74e85bed87a68e10d07cd3885131
Abhishek Mishra <95057637+abhishek-mishraji@users.noreply.github.com>
2024-11-17 16:01:17 +0530

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -26,7 +26,7 @@
                 <li><a href="./blog.php">Home</a></li>
                 <li><a href="./blog.php">Blog</a></li>
                 <li><a href="./about.php">About</a></li>
-                <li><a href="./services.php">Services</a></li>
+<!--                 <li><a href="./services.php">Services</a></li> -->
                 <li><a href="./contact.php">Contact</a></li>
             </ul>
         </article>
